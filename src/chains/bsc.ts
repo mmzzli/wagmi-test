3a3239024b72b7b77807725193ae46c578e3649a
jacob <jacob@jacobdeMacBook-Pro.local>
2025-05-10 20:23:36 +0800

    fix: 🐛 fix rpc

---
+++ src/chains/bsc.ts
@@ -0,0 +1,29 @@
+import { defineChain } from "viem";
+
+export const bsc = /*#__PURE__*/ defineChain({
+  id: 56,
+  name: "BNB Smart Chain",
+  nativeCurrency: {
+    decimals: 18,
+    name: "BNB",
+    symbol: "BNB",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://go.getblock.io/bc841869c5bc477b8c57802853f6698a"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "BscScan",
+      url: "https://bscscan.com",
+      apiUrl: "https://api.bscscan.com/api",
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+      blockCreated: 15921452,
+    },
+  },
+});
