5e947363fd8915e138dda4f833eebcf4d1ed8d82
jacob <jacob@jacobdeMacBook-Pro.local>
2025-06-02 09:37:07 +0800

    fix: 🐛 fix port

--- src/service/black.ts
+++ src/service/black.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const getList = async () => {
-  return axios.get("http://localhost:3000/black/info");
+  return axios.get("http://18.163.63.131:3001/black/info");
 };
 
 export const searchBlack = async (address: string) => {
-  return axios.get(`http://localhost:3000/black/search?address=${address}`);
+  return axios.get(`http://18.163.63.131:3001/black/search?address=${address}`);
 };
